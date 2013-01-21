a73375d14e02691323f28292afebc9614a9f2ab5
Florin T.PATRASCU <florin.patrascu@gmail.com>
2013-01-20 20:57:30 -0500

    Micro 0.2 and the updated local repo

---
+++ run_eclipse.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Start Micro with the embedded Jetty server.
+
+java $OPT -cp $( echo WEB-INF/lib/*.jar WEB-INF/classes/*.xml . | sed 's/ /:/g') ca.simplegames.MicroAtHeroku . $1 $2 $3 $4 $5 $6
