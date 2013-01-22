12d8129ce3a1002e5d8c5ef9eb15869c01c51e5c
Florin T.PATRASCU <florin.patrascu@gmail.com>
2013-01-22 18:57:58 -0500

    corrected the syntax of the run.sh file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Start Micro with the embedded Jetty server.
-$OPT=-Dnet.sf.ehcache.skipUpdateCheck=true -Xmx384m -Xss512k -XX:+UseCompressedOops
+OPT='-Dnet.sf.ehcache.skipUpdateCheck=true -Xmx384m -Xss512k -XX:+UseCompressedOops'
 java $OPT -cp $( echo libs/*.jar classes/*.xml . | sed 's/ /:/g') ca.simplegames.micro.WebServer web 8080 $1 $2 $3 $4 $5 $6
