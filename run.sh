5294de879ac5962dc7e8b6cb838184054f64b28d
Florin T.PATRASCU <florin.patrascu@gmail.com>
2013-01-27 00:14:03 -0500

    testing with the classes back into the WEB-INF

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Start Micro with the embedded Jetty server.
 OPT='-Dnet.sf.ehcache.skipUpdateCheck=true -Xmx384m -Xss512k -XX:+UseCompressedOops'
-java $OPT -cp $( echo libs/*.jar classes/*.xml . | sed 's/ /:/g') ca.simplegames.micro.WebServer web 8080 $1 $2 $3 $4 $5 $6
+java $OPT -cp $( echo libs/*.jar . | sed 's/ /:/g') ca.simplegames.micro.WebServer web 8080 $1 $2 $3 $4 $5 $6
